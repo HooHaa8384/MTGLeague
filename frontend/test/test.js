02644918905704d2a589534c97a46b9dc0ec546d
HooHaa8384 <Hoohaa838@gmail.com>
2017-01-05 21:50:35 -0500

    Npm (#3)
    * adding jasmine-phantom
    
    * getting the build to work

--- frontend/test/test.js
+++ frontend/test/test.js
@@ -1,5 +1,11 @@
+require('../src/test.js');
+
 describe("A suite", () => {
   it("contains spec with an expectation", () => {
     expect(true).toBe(true);
   });
+
+  /*it("returns true", () => {
+    expect(isTrue()).toBe(true);
+  });*/
 });
